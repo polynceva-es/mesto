89044f2d3e7d41ca1134eab42f8585aa23eda3eb
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2022-12-24 17:40:53 +0300

    added classes Card and FormValidator

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,45 @@
+import { Card } from './Card.js';
+import { initialCards } from "./InitialCards.js";
+import { FormValidator } from './FormValidator.js';
+import {validationConfig} from './ValidationConfig.js';
+
+//модальные окна
 const popupEditProfile = document.querySelector('.popup_type_form-editprofile');
 const popupAddCard = document.querySelector('.popup_type_form-addcard');
 const popupImage = document.querySelector('.popup_type_image');
+//формы
 const formEditProfile = popupEditProfile.querySelector('.popup__form_edit');
 const formAddCard = popupAddCard.querySelector('.popup__form_add');
+//инпуты
 const inputName = formEditProfile.querySelector('.popup__input_type_name');
 const inputAbout = formEditProfile.querySelector('.popup__input_type_about');
 const inputTitle = formAddCard.querySelector('.popup__input_type_title');
 const inputUrl = formAddCard.querySelector('.popup__input_type_url');
-const popupImageElement = popupImage.querySelector('.popup__image');
-const popupFigcaptionElement = popupImage.querySelector('.popup__figcaption');
+//кнопки на странице
 const buttonEditProfile = document.querySelector('.button_type_edit');
 const buttonAddCard = document.querySelector('.button_type_add');
+//информация профиля на странице
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
+//массив кнопок "закрыть"
 const closeButtons = Array.from(document.querySelectorAll('.button_type_close'));
-const templateElement = document.querySelector('.template').content;
-const elementsSection = document.querySelector('.elements');
+//массивы инпутов для каждой формы
 const inputListformEditProfile = Array.from(formEditProfile.querySelectorAll('.popup__input'));
 const inputListformAddCard = Array.from(formAddCard.querySelectorAll('.popup__input'));
+//кнопки сабмита
 const buttonSubmitFormEditProfile = formEditProfile.querySelector('.button_type_submit');
 const buttonSubmitFormAddCard = formAddCard.querySelector('.button_type_submit');
 
-//функция нажатия на кнопку Like
-function addLike(evt) {
-  evt.target.classList.toggle('button_active');
-};
+//отрисовка стартовых карточек на странице
+initialCards.forEach((item) => {
+  addCard(item);
+});
 
-//функция нажатия на кнопку Удалить
-function deleteCard(evt) {
-  evt.target.closest('.card').remove();
-};
+//включение валидации форм
+const formEditProfileValidator = new FormValidator(validationConfig, formEditProfile);
+formEditProfileValidator.enableValidation();
+const formAddCardValidator = new FormValidator(validationConfig, formAddCard);
+formAddCardValidator.enableValidation();
 
 //функция открытия поп-апа
 function openPopup(popupElement) {
@@ -43,42 +53,12 @@ function closePopup(popupElement) {
   popupElement.classList.remove('popup_opened');
 };
 
-//функция открытия попапа с картинкой
-function openPopupWithImage(evt) {
-  const url = evt.target.src;
-  const name = evt.target.alt;
-  popupImageElement.src = url;
-  popupImageElement.alt = name;
-  popupFigcaptionElement.textContent = name;
-  openPopup(popupImage);
-};
-
-//функция генерации карточки
-function generateCard(card) {
-  const cardElement = templateElement.querySelector('.card').cloneNode(true);
-  const cardImage = cardElement.querySelector('.card__image');
-  const cardCaption = cardElement.querySelector('.card__caption');
-  cardImage.src = card.url;
-  cardImage.alt = card.name;
-  cardCaption.textContent = card.name;
-  const buttonLike = cardElement.querySelector('.button_type_like');
-  const buttonDelete = cardElement.querySelector('.button_type_delete');
-  buttonLike.addEventListener('click', addLike);
-  buttonDelete.addEventListener('click', deleteCard);
-  cardImage.addEventListener('click', openPopupWithImage);
-  return cardElement;
-};
-
 //функция добавления карточки на страницу
-function addCard(card) {
-  const cardElement = generateCard(card);
-  elementsSection.prepend(cardElement);
+function addCard(object) {
+  const card = new Card(object, '.template', openPopup);
+  const cardElement = card.generateCard();
+  document.querySelector('.elements').prepend(cardElement);
 };
-
-//вызов функция отрисовки стартовых карточек на странице
-initialCards.forEach(function (card) {
-  addCard(card);
-});
 
 //функция закрытия поп-апа по клику на overlay
 function closePopupClickOnOverlay(evt, popupElement) {
@@ -96,14 +76,14 @@ function closePopupPressEsc(evt) {
 };
 
 //функция обработчик события нажатия кнопки Закрыть
-function handleButtonСloseClick(evt) {
+function handleButtonCloseClick(evt) {
   const popupElement = evt.target.closest('.popup');
   closePopup(popupElement);
 };
 
 //для каждой кнопки Закрыть добавим слушатель события 'клик'
 closeButtons.forEach(function (buttonClose) {
-  buttonClose.addEventListener('click', handleButtonСloseClick);
+  buttonClose.addEventListener('click', handleButtonCloseClick);
 });
 
 //функция заполнения значений полей ввода из разметки страницы (форма редактировать профиль)
@@ -115,8 +95,7 @@ function inputInfo() {
 //функция открытия формы Редактировать профиль
 function openFormEditProfile() {
   inputInfo();
-  removeValidationErrors (formEditProfile, inputListformEditProfile, validationConfig);
-  disabledButtonSubmit(inputListformEditProfile, buttonSubmitFormEditProfile, validationConfig);
+  formEditProfileValidator.resetFormElementValidationState(inputListformEditProfile, buttonSubmitFormEditProfile);//
   openPopup(popupEditProfile);
 };
 
@@ -130,22 +109,21 @@ function submitEditProfileForm(evt) {
 //функция открытия формы Добавить карточку
 function openFormAddCard() {
   formAddCard.reset();
-  removeValidationErrors (formAddCard, inputListformAddCard, validationConfig);
-  disabledButtonSubmit(inputListformAddCard, buttonSubmitFormAddCard, validationConfig);
+  formAddCardValidator.resetFormElementValidationState(inputListformAddCard, buttonSubmitFormAddCard);//
   openPopup(popupAddCard);
 };
 
 //функция нажатия на кнопку Создать
 function addCardElement(evt) {
-  const card = {
+  const object = {
     name: inputTitle.value,
     url: inputUrl.value,
   };
-  addCard(card);
+  addCard(object);
   closePopup(popupAddCard);
   formAddCard.reset();
 };
-
+//слушатели событий
 buttonEditProfile.addEventListener('click', openFormEditProfile);
 buttonAddCard.addEventListener('click', openFormAddCard);
 formEditProfile.addEventListener('submit', submitEditProfileForm);
