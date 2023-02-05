474b551990239e89dc78901b8c427b8d70e7f48f
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-02-05 11:42:41 +0300

    added likes number

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -2,6 +2,7 @@ export class Card {
   constructor (cardData, templateSelector, handleOpenPopup, handleOpenPopupDeleteCard) {
     this._caption = cardData.name;
     this._imageLink = cardData.link;
+    this._likeNum = cardData.likes.length;
     this._templateSelector = templateSelector;
     this.handleOpenPopup = handleOpenPopup;
     this.handleOpenPopupDeleteCard = handleOpenPopupDeleteCard;
@@ -17,11 +18,13 @@ export class Card {
     this._cardImage = this._cardElement.querySelector('.card__image');
     this._cardCaption = this._cardElement.querySelector('.card__caption');
     this._buttonLike = this._cardElement.querySelector('.button_type_like');
+    this._likeNumConteiner = this._cardElement.querySelector('.card__like_num');
     this._buttonDelete = this._cardElement.querySelector('.button_type_delete');
     this._setEventListeners();
     this._cardImage.src = this._imageLink;
     this._cardImage.alt = this._caption;
     this._cardCaption.textContent = this._caption;
+    this._likeNumConteiner.textContent = this._likeNum;
     return this._cardElement;
   }
 //установка слушателей событий
